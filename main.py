6269438bcd2f243df1ce7f84827fb0fd45905d4d
Kaushik <kaushik.kommineni@gmail.com>
2025-07-01 15:00:28 -0500

    Secure features added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 df=pd.read_csv("hotels.csv",dtype={"id":str})
+df_card=pd.read_csv("cards.csv",dtype=str).to_dict(orient="records")
+df_secure=pd.read_csv("card_security.csv",dtype=str)
 
 
 class Hotel:
@@ -31,13 +33,41 @@ class Reservation:
         Hotel: {self.hotel.name}
         """
         return content
+
+
+class CreditCard():
+    def __init__(self,number):
+        self.number=number
+    def validate(self,expiration,cvc,holder):
+        card_reader={"number" :self.number, "expiration":expiration,"cvc":cvc,"holder":holder}
+        if card_reader in df_card:
+            return True
+        else :
+            return False
+
+class Securecard(CreditCard):
+    def authenticate(self,password):
+        passowrd=df_secure.loc[df_secure["number"]==self.number,"password"].squeeze()
+        if (passowrd == password):
+            return True
+        else:
+            return False
+
+
 print(df)
 hotel_id=input("Enter the Hotel ID you want to book :")
 hotel=Hotel(hotel_id)
 if hotel.available():
-    hotel.book()
-    name=input("Enter Your Name :")
-    reservation_ticket= Reservation(name,hotel)
-    print(reservation_ticket.generate())
+    credit=Securecard(number="1234")
+    if credit.validate(expiration="12/26",cvc="123",holder="JOHN SMITH"):
+        if credit.authenticate(password="mypass"):
+            hotel.book()
+            name=input("Enter Your Name :")
+            reservation_ticket= Reservation(name,hotel)
+            print(reservation_ticket.generate())
+        else:
+            print("Your card Password is Wrong ")
+    else:
+        print("There is problem in Your card")
 else:
     print("Hotel is not empty")
